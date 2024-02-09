c05fa85ed13ec46bb1f28302558adac531d43e92
newerk <juniusowens@yahoo.com>
2024-02-09 14:17:05 -0500

    Remove File Extension Requirement From URL

--- index.js
+++ index.js
@@ -4,18 +4,31 @@ const path = require("path");
 
 const server = http
   .createServer((req, res) => {
-    const filePath = path.join(
-      __dirname,'pages',
-      req.url === "/" ? "index.html" : req.url
-    );
+    let pageURL;
+    switch (req.url) {
+      case "/":
+        pageURL = "index.html";
+        break;
+      case "/about":
+        pageURL = "about.html";
+        break;
+      case "/contact-me":
+        pageURL = "contact-me.html";
+        break;
+    }
+
+    const filePath = path.join(__dirname, "pages", pageURL);
 
     fs.readFile(filePath, (err, content) => {
       if (err) {
-        fs.readFile(path.join(__dirname, 'pages', "404.html"), (err, content) => {
-          if (err) throw err;
-          res.writeHead(404, { "Content-Type": "text/html" });
-          res.end(content);
-        });
+        fs.readFile(
+          path.join(__dirname, "pages", "404.html"),
+          (err, content) => {
+            if (err) throw err;
+            res.writeHead(404, { "Content-Type": "text/html" });
+            res.end(content);
+          }
+        );
       } else {
         res.writeHead(200, { "Content-Type": "text/html" });
         res.end(content);
