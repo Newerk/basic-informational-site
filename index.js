47c0b361717f5ae85bd480cd9ac25e521c92e0be
newerk <juniusowens@yahoo.com>
2024-02-09 14:36:16 -0500

    Move Condtion for 404 Error Page into Switch/Case

--- index.js
+++ index.js
@@ -9,30 +9,26 @@ const server = http
       case "/":
         pageURL = "index.html";
         break;
+
       case "/about":
         pageURL = "about.html";
         break;
+
       case "/contact-me":
         pageURL = "contact-me.html";
+        break;
+
+      default:
+        pageURL = "404.html";
         break;
     }
 
     const filePath = path.join(__dirname, "pages", pageURL);
 
     fs.readFile(filePath, (err, content) => {
-      if (err) {
-        fs.readFile(
-          path.join(__dirname, "pages", "404.html"),
-          (err, content) => {
-            if (err) throw err;
-            res.writeHead(404, { "Content-Type": "text/html" });
-            res.end(content);
-          }
-        );
-      } else {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(content);
-      }
+      if (err) throw err;
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(content);
     });
   })
   .listen("8080", () => console.log("Running server on port 8080..."));
